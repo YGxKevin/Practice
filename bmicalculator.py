276aab177e412700931fe7813be4a3521f0c2453
YGxKevin <47622079+YGxKevin@users.noreply.github.com>
2019-06-09 09:04:29 +0800

    Update bmicalculator.py

--- bmicalculator.py
+++ bmicalculator.py
@@ -8,7 +8,9 @@ weight = float(weight)
 bmi = weight / (height*height)
 if bmi < 18.5:
 	print('your bmi is too low.')
-if 18.5 <= bmi < 25:
+elif 18.5 <= bmi < 25:
 	print('your bmi is normal.')
-if bmi >= 25:
+elif bmi >= 25:
 	print('your bmi is too high.')
+else:
+	print('please reenter valid figure)
