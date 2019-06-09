fd48cc0d4ce11461a68c62ed194bbf66abee5b9d
YGxKevin <47622079+YGxKevin@users.noreply.github.com>
2019-06-09 08:50:29 +0800

    Add files via upload

---
+++ bmicalculator.py
@@ -0,0 +1,13 @@
+print('Hi This is a BMI calculator')
+name = input('name: ')
+height = input('height(m): ')
+weight = input('weight(kg): ')
+height = float(height)
+weight = float(weight)
+bmi = weight / (height*height)
+if bmi < 18.5:
+	print('your bmi is too low.')
+if 18.5 <= bmi < 25:
+	print('your bmi is normal.')
+if bmi >= 25:
+	print('your bmi is too high.')
